cf5606c8370bbdb313f74e73542f6e689741d040
zalaw <keep_razvy@yahoo.com>
2023-01-10 01:23:29 +0200

    Bugfix

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const firebaseConfigDev = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY_DEV,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN_DEV,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_DEV,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID_DEV,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID_DEV,
+};
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
@@ -12,7 +21,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(process.env.NODE_ENV === "development" ? firebaseConfigDev : firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
